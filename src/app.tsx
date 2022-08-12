17cc3b1c9c8778006720c69c6dcd6d4e3e05b634
ishino-kenta <berry.peach.97@gmail.com>
2022-08-12 08:00:49 +0000

    Initial commit.

--- src/app.tsx
+++ src/app.tsx
@@ -1,45 +1,73 @@
-import * as React from 'react';
-import {createRoot} from 'react-dom/client';
+import { Item, Rect, Shape } from "@mirohq/websdk-types";
 
-async function addSticky() {
-  const stickyNote = await miro.board.createStickyNote({
-    content: 'Hello, World!',
-  });
-
-  await miro.board.viewport.zoomTo(stickyNote);
+export function makeDeck(selection: Item[]) {
+    gather(selection);
+    shuffle(selection);
+    cover(selection);
 }
 
-function App() {
-  React.useEffect(() => {
-    addSticky();
-  }, []);
+async function gather(items: Item[]) {
+    const viewport: Rect = await miro.board.viewport.get();
+    const centerX = viewport.x + viewport.width / 2;
+    const centerY = viewport.y + viewport.height / 2;
 
-  return (
-    <div className="grid wrapper">
-      <div className="cs1 ce12">
-        <img src="/src/assets/congratulations.png" alt="" />
-      </div>
-      <div className="cs1 ce12">
-        <h1>Congratulations!</h1>
-        <p>You've just created your first Miro app!</p>
-        <p>
-          To explore more and build your own app, see the Miro Developer
-          Platform documentation.
-        </p>
-      </div>
-      <div className="cs1 ce12">
-        <a
-          className="button button-primary"
-          target="_blank"
-          href="https://developers.miro.com"
-        >
-          Read the documentation
-        </a>
-      </div>
-    </div>
-  );
+    for (let item of items) {
+        if (!isShape(item)) {
+            return;
+        }
+        item.x = centerX - item.width / 2;
+        item.y = centerY - item.height / 2;
+        item.sync();
+    }
 }
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+function isShape(item: Item): item is Shape {
+    if (!('type' in item)) {
+        return false
+    }
+    return typeof item === 'object' &&
+        item.type == 'shape';
+}
+
+async function shuffle(items: Item[]) {
+    for (let i = items.length - 1; i >= 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        let tmp = items[i];
+        items[i] = items[j];
+        items[j] = tmp;
+    }
+    for (let item of items) {
+        miro.board.bringToFront(item);
+    }
+}
+
+async function cover(items: Item[]) {
+    const viewport: Rect = await miro.board.viewport.get();
+    const centerX = viewport.x + viewport.width / 2;
+    const centerY = viewport.y + viewport.height / 2;
+
+    for (let item of items) {
+        if (isShape(item)) {
+            const sample: Shape = item;
+            const width: number = sample.width * 1.2;
+            const height: number = sample.height;
+            const x: number = centerX - width / 2 + sample.width * 0.1;
+            const y: number = centerY - height / 3
+
+            const shape: Shape = await miro.board.createShape({
+                content: '<p>Deck of cards</p>',
+                shape: 'rectangle',
+                "style": {
+                    "fillColor": "#13E3EB"
+                },
+                x: x,
+                y: y,
+                width: width,
+                height: height
+            });
+
+            miro.board.bringToFront(shape);
+        }
+        break;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
+import { Item } from "@mirohq/websdk-types";
+import { makeDeck } from "./app";
+
 async function init() {
-  miro.board.ui.on('icon:click', async () => {
-    await miro.board.ui.openPanel({url: 'app.html'});
-  });
+    miro.board.ui.on('icon:click', async () => {
+        const selection: Item[] = await miro.board.getSelection();
+
+        if (selection.length) {
+            makeDeck(selection);
+        } else {
+            await miro.board.ui.openPanel({ url: 'panel.html' });
+        }
+    });
 }
 
 init();
